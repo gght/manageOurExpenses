497aef4b674ac54f2a12de57733bffa9d1a12f22
SubaruNakayama <s.nakayama@SubaruNakayamas-MacBook-Pro.local>
2017-12-24 09:37:16 +0900

    1224commit

---
+++ src/Hoge.java
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="1561.1">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 14.0px Osaka; color: #666666; background-color: #ffffff}
+    span.s1 {font-kerning: none}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">Public class Home {</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">  </span>public static void main(String[] args){</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>System.out.println(“こんにちは”);</span></p>
+<p class="p1"><span class="s1">　}</span></p>
+<p class="p1"><span class="s1">}</span></p>
+</body>
+</html>
